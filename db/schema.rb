1536904713f6d2133d62fb9dd22840f875b57951
Isaac <iyoon22@gmail.com>
2020-04-28 22:13:12 -0700

    Update personal log and migrate servers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_034141) do
+ActiveRecord::Schema.define(version: 2020_04_29_044057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "servers", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_servers_on_owner_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
