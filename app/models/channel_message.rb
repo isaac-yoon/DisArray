8d71fc180cecdf2ebe0ffe57804863690c8ef061
Isaac <iyoon22@gmail.com>
2020-05-06 00:15:52 -0700

    Create a model, controller, and channel for channel messages

---
+++ app/models/channel_message.rb
@@ -0,0 +1,2 @@
+class ChannelMessage < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_211112) do
+ActiveRecord::Schema.define(version: 2020_05_06_070757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2020_05_02_211112) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "channel_messages", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "author_id", null: false
+    t.integer "channel_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_channel_messages_on_author_id"
+    t.index ["channel_id"], name: "index_channel_messages_on_channel_id"
   end
 
   create_table "channels", force: :cascade do |t|
