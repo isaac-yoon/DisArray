a0dc9efe39abd0c7b0bf5534ee9a78c5ab3a35cd
Isaac <iyoon22@gmail.com>
2020-06-15 17:01:57 -0700

    Remove unnecessary code and refactor

--- frontend/chat/chat_room.jsx
+++ frontend/chat/chat_room.jsx
@@ -45,44 +45,6 @@ class ChatRoom extends React.Component {
 
   componentDidUpdate(prevProps) {
     if (this.bottom.current !== null) this.bottom.current.scrollIntoView();
-
-    // if the URL changes
-
-    if (prevProps.location.pathname !== this.props.location.pathname) {
-      this.setState(
-        {messages: [], channelId: this.props.location.pathname.split('/')[4] || ''},
-        () => {
-          // unsubscribe to channel if the pathname changes
-          App.cable.disconnect()
-          App.cable.subscriptions.create(
-            {
-              channel: "ChatChannel",
-              channelId: this.state.channelId,
-            },
-            // this needs to match chat_channel.rb
-            {
-              received: data => {
-                switch (data.type) {
-                  case 'message':
-                    this.setState({
-                      messages: this.state.messages.concat(data.message)
-                    });
-                    break;
-                }
-              },
-              // received will be invoked when the subscription broadcasts from the backend
-              speak: function (data) {
-                return this.perform("speak", data)
-              },
-              load: function () {
-                return this.perform("load")
-              }
-              // performs the speak method in the backend while passing in some data
-            }
-          );
-        }
-      )
-    }
   }
 
   componentWillUnmount() {
