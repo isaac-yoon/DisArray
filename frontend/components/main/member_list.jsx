398787b8ca343d10e97b95849a3c8a308e99bdb9
Isaac <iyoon22@gmail.com>
2020-06-26 00:08:41 -0700

    Render all users of DisArray onto page

--- frontend/components/main/member_list.jsx
+++ frontend/components/main/member_list.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const MemberList = (props) => {
+  debugger
   return (
     <div>
-
+      {/* format members so that every member is on a separate line */}
+      { props.members.length > 0 ? props.members.map((member) => member.username ) : null }
     </div>
   )
 };
--- frontend/components/main/member_list_container.js
+++ frontend/components/main/member_list_container.js
@@ -3,14 +3,9 @@ import MemberList from './member_list';
 
 const mapStateToProps = (state, ownProps) => {
   return {
+    // add a selector that selects members based on memberIds and if it matches the servermemberships table
     members: Object.values(state.entities.users),
   }
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-
-  }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(MemberList);
+export default connect(mapStateToProps, null)(MemberList);
