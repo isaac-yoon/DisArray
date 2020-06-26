98a8f0d141f8ef8bf27faade3678d8ffe3711845
Isaac <iyoon22@gmail.com>
2020-06-25 23:58:48 -0700

    Add member list files

---
+++ frontend/components/main/member_list.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MemberList = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+};
+
+export default MemberList;
---
+++ frontend/components/main/member_list_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import MemberList from './member_list';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    members: ,
+  }
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MemberList);
