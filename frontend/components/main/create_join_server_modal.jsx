87fb569437c6bbd70dd4ac415c92e1b8711e228c
Isaac <iyoon22@gmail.com>
2020-06-29 14:15:39 -0700

    Add join-server and create-server icons

--- frontend/components/main/create_join_server_modal.jsx
+++ frontend/components/main/create_join_server_modal.jsx
@@ -20,13 +20,13 @@ const CreateJoinServerModal = (props) => {
       <div className = "create-join-server-modal-options-container">
         <div id = "create-join-actions-container">
           <div className = "actions-links" id = "create-server-link">
-            Create a new server and invite your friends. It's free!
+            <b>Create</b> a new server and invite your friends. It's free!
             {/* picture */}
             <div>Create a Server</div>
           </div>
 
           <div className = "actions-links" id = "join-server-link">
-            Join a server with an invite code or link.
+            <b>Join</b> a server with an invite code or link.
             {/* picture */}
             <div>Join a server</div>
           </div>
