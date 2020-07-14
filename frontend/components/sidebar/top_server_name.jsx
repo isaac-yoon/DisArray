0b824d029eb31f7a3a2d7935c64ab6882df166ef
Isaac <iyoon22@gmail.com>
2020-07-13 22:05:10 -0700

    Add a dropdown button (frontend only) next to server name

--- frontend/components/sidebar/top_server_name.jsx
+++ frontend/components/sidebar/top_server_name.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const TopServerName = (props) => {
   return (
       <div className="side-bar-server-name">
-        { props.servers[props.match.params.server_id] ? props.servers[props.match.params.server_id].name.toUpperCase() : null }
+        <div>
+          { props.servers[props.match.params.server_id] ? props.servers[props.match.params.server_id].name.toUpperCase() : null }
+        </div>
+        <label id="server-name-dropdown">⌄</label>
       </div> 
   )
 };
