a84f6501ec57e4641c16abbebfff68ffd25b6d05
Isaac <iyoon22@gmail.com>
2020-07-30 22:53:42 -0700

    Change login and signup buttons to link to their respective functions and remove modal functionality

--- frontend/components/splash_page/splash_nav_bar.jsx
+++ frontend/components/splash_page/splash_nav_bar.jsx
@@ -4,17 +4,6 @@ import { Link } from 'react-router-dom';
 const SplashNavBar = ({ currentUser, logout, openModal }) => {
   return (
     <div className = "main-nav-bar">
-
-      {/* <div className = "main-nav-bar-discordicon">
-        <Link to="/">
-          <img 
-            src={window.discordiconbackground}
-            alt="discord-icon-background"
-            id="main-nav-bar-discordiconbackground"
-          />
-        </Link>
-      </div> */}
-
       <div className = "main-nav-bar-middle">
         <a href="https://www.linkedin.com/in/lucidlaughter/">
           <img 
--- frontend/components/splash_page/welcome_nav_bar.jsx
+++ frontend/components/splash_page/welcome_nav_bar.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const WelcomeNavBar = ({ openModal }) => {
+const WelcomeNavBar = () => {
+
+  // reminder: there are still login, demo, and signup modals in the file tree
+  // they were removed and changed to link to the login/signup pages instead for a cleaner user experience
+  
   return (
     <div
       className = "welcome-nav-bar-login-signup-links"
     >
       <nav className="login-signup">
-        <button
-          id="welcome-login-button"
-          onClick={() => openModal('login')}
-        >LOGIN</button>
+        <Link to="login">
+          <button
+            id="welcome-login-button"
+          >LOGIN</button>
+        </Link>
 
-        <button
-          id="welcome-signup-button"
-          onClick={() => openModal('signup')}
-        >SIGNUP</button>
+        <Link to="register">
+          <button
+            id="welcome-signup-button"
+          >SIGNUP</button>  
+        </Link>
       </nav>
     </div>
   )
