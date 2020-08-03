47ab6d65ed7f2931364443dcb9d7d86d9a756b83
Isaac <iyoon22@gmail.com>
2020-08-03 11:20:56 -0700

    Change footer message

--- frontend/components/splash_page/splash_footer.jsx
+++ frontend/components/splash_page/splash_footer.jsx
@@ -8,7 +8,7 @@ const SplashFooter = () => {
         <h2 id="splash-footer-message-1">Ready to try DisArray? It's free!</h2>
         <br/>
         <h3 id="splash-footer-message-2">JOIN OVER 5 USERS TODAY!</h3>
-        <h6>DisArray is a clone of Discord.</h6>
+        <h6>DisArray is a clone inspired by Discord.</h6>
       </div>
 
       <div className="splash-footer-signup-link">
