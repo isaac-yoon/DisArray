989a16815d19dfb9e52805d917685c1d2f2c9219
Isaac <iyoon22@gmail.com>
2021-02-02 13:26:17 -0800

    Change member_id inputs and fix typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,50 +17,43 @@ Users = User.create([
   { username: 'Black Widow', email: 'blackwidow@avengers.io', password: '123456' },
 ])
 
-isaac = Users[0]
-demologin = Users[1]
-captain_america = Users[2]
-tony_stark = Users[3]
-bruce_banner = Users[4]
-natasha = Users[5]
-
 Server.destroy_all
 Servers = Server.create([
-  { name: 'Avengers', owner_id: tony_stark, invite_code: 'ISA662' },
-  { name: 'Stark Industries', owner_id: tony_stark, invite_code: 'PUBLIC22' },
+  { name: 'Avengers', owner_id: User.all[3].id, invite_code: 'ISA662' },
+  { name: 'Stark Industries', owner_id: User.all[3].id, invite_code: 'PUBLIC22' },
 ])
 
 ServerMembership.destroy_all
 ServerMembership = ServerMembership.create([
   # avengers server
-  { member_id: isaac.id, server_id: Server.first.id },
-  { member_id: demologin.id, server_id: Server.first.id },
-  { member_id: captain_america.id, server_id: Server.first.id },
-  { member_id: tony_stark.id, server_id: Server.first.id },
-  { member_id: bruce_banner.id, server_id: Server.first.id },
-  { member_id: natasha.id, server_id: Server.first.id },
+  { member_id: User.all[0].id, server_id: Server.first.id },
+  { member_id: User.all[1].id, server_id: Server.first.id },
+  { member_id: User.all[2].id, server_id: Server.first.id },
+  { member_id: User.all[3].id, server_id: Server.first.id },
+  { member_id: User.all[4].id, server_id: Server.first.id },
+  { member_id: User.all[5].id, server_id: Server.first.id },
 
   # stark industries server
-  { member_id: tony_stark.id, server_id: Server.second.id },
-  { member_id: isaac.id, server_id: Server.second.id },
-  { member_id: demologin.id, server_id: Server.second.id },
+  { member_id: User.all[3].id, server_id: Server.second.id },
+  { member_id: User.all[0].id, server_id: Server.second.id },
+  { member_id: User.all[1].id, server_id: Server.second.id },
 ])
 
 ChannelMessage.destroy_all
 ChannelMessage = ChannelMessage.create([
   # Stark Industries General Channel Messages
-  { author_id: tony_stark.id, body: 'The truth is...I am Iron Man.', channel_id: Channel.second.id, created_at: Time.zone.parse('2011-02-03 15:30:45')},
-  { author_id: demologin.id, body: 'What?!', channel_id: Channel.second.id, created_at: Time.zone.parse('2011-02-03 15:30:48')},
-  { author_id: isaac.id, body: 'Yep. Almost all the reporters went crazy.', channel_id: Channel.second.id, created_at: Time.zone.parse('2011-02-03 15:30:49')},
-  { author_id: tony_stark.id, body: "It's not about me. It's not about you. It's not even about us. It's about legacy. It's about what we choose to leave behind.", channel_id: Channel.second.id, created_at: Time.zone.parse('2014-01-02 18:30:45')},
+  { author_id: User.all[3].id, body: 'The truth is...I am Iron Man.', channel_id: Channel.second.id, created_at: Time.zone.parse('2011-02-03 15:30:45')},
+  { author_id: User.all[1].id, body: 'What?!', channel_id: Channel.second.id, created_at: Time.zone.parse('2011-02-03 15:30:48')},
+  { author_id: User.all[0].id, body: 'Yep. Almost all the reporters went crazy.', channel_id: Channel.second.id, created_at: Time.zone.parse('2011-02-03 15:30:49')},
+  { author_id: User.all[3].id, body: "It's not about me. It's not about you. It's not even about us. It's about legacy. It's about what we choose to leave behind.", channel_id: Channel.second.id, created_at: Time.zone.parse('2014-01-02 18:30:45')},
 
   # Avengers General Channel Messages
-  { author_id: tony_stark.id, body: "We're the avengers. We can bust arms dealers all the livelong day, but that up there, that's...the end game. How were you guys planning on beating them?", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:33')},
-  { author_id: captain_america.id, body: "Together.", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:34')},
-  { author_id: tony_stark, body: "We'll lose.", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:35')},
-  { author_id: captain_america.id, body: "Then we'll do that together too.", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:36')},
+  { author_id: User.all[3].id, body: "We're the avengers. We can bust arms dealers all the livelong day, but that up there, that's...the end game. How were you guys planning on beating them?", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:33')},
+  { author_id: User.all[2].id, body: "Together.", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:34')},
+  { author_id: User.all[3].id, body: "We'll lose.", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:35')},
+  { author_id: User.all[2].id, body: "Then we'll do that together too.", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:36')},
 
   # Direct Messages
-  # { author_id: captain_america.id, body: "", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:36')},
+  # { author_id: captain_america.id, body: "Tony, I'm glad you're back at the compound. I don't like the idea of you rattling around a mansion by yourself. We all need family. The Avengers are yours, maybe more so than mine. I've been on my own since I was 18. I never really fit in anywhere, even in the army. My faith's in people, I guess. Individuals. And I'm happy to say that, for the most part, they haven't let me down. Which is why I can't let them down either. Locks can be replaced, but maybe they shouldn't. I know I hurt you, Tony. I guess I thought by not telling you about your parents I was sparing you, but I can see now that I was really sparing myself, and I'm sorry. Hopefully one day you can understand. I wish we agreed on the Accords, I really do.I know you're doing what you believe in, and that's all any of us can do. That's all any of us should do. So, no matter what. I promise you, if you need us, if you need me, I'll be there.", channel_id: Channel.first.id, created_at: Time.zone.parse('2012-05-04 09:30:36')},
 
 ])
