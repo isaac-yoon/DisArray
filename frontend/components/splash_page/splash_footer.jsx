c68573c6d30930a6fd6ed1391c91e8c40c67d6e1
Isaac <iyoon22@gmail.com>
2021-02-11 04:01:22 -0800

    Color warning sign red

--- frontend/components/splash_page/splash_footer.jsx
+++ frontend/components/splash_page/splash_footer.jsx
@@ -12,7 +12,7 @@ const SplashFooter = () => {
         <br/>
         <h3 id="splash-footer-message-2">JOIN OVER 5 USERS TODAY!</h3>
         <h6>DisArray is a clone inspired by Discord.</h6>
-        <h4>WARNING: This site is not secure. Do not put in any personal information.</h4>
+        <h4 id="warning">WARNING: This site is not secure. Do not put in any personal information.</h4>
       </div>
 
       <div className="splash-footer-signup-link">
