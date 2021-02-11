5b6d1b250b6a2622581096e0eeb6b3fff2c850d3
Isaac <iyoon22@gmail.com>
2021-02-11 03:42:40 -0800

    Remove github and linkedin links from splash page

--- frontend/components/splash_page/splash_nav_bar.jsx
+++ frontend/components/splash_page/splash_nav_bar.jsx
@@ -4,30 +4,6 @@ import { Link } from 'react-router-dom';
 const SplashNavBar = ({ currentUser, logout, openModal }) => {
   return (
     <div className = "main-nav-bar">
-      <div className = "main-nav-bar-middle">
-        <a href="https://www.linkedin.com/in/lucidlaughter/">
-          <img 
-            src={window.linkedin}
-            alt="linked-in-icon"
-            className="main-nav-bar-icons"
-            id="linked-in-icon"
-          />
-        </a>
-
-        &nbsp;
-        &nbsp;
-        &nbsp;
-
-        <a href="https://github.com/isaac-yoon">
-          <img 
-            src={window.github}
-            alt="github-in-icon"
-            className="main-nav-bar-icons"
-            id="github-icon"
-          />
-        </a>
-      </div>
-
       <div className = "main-nav-bar-login-button-container">
         <Link to="/login" id="main-nav-bar-login-link">
           <button 
