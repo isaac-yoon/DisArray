25a7fd099c4dbb6091a7fc38909620e1274bca25
Isaac <iyoon22@gmail.com>
2022-06-15 01:22:54 -0700

    Clean up footer of splash page

--- frontend/components/splash_page/splash_footer.jsx
+++ frontend/components/splash_page/splash_footer.jsx
@@ -12,10 +12,9 @@ const SplashFooter = () => {
         <br/>
         <h3 id="splash-footer-message-2">JOIN OVER 5 USERS TODAY!</h3>
         <h6>DisArray is a clone inspired by Discord.</h6>
-        <h4 id="warning">WARNING: This site is not secure. Do not put in any personal information.</h4>
       </div>
 
-      <div className="splash-footer-signup-link">
+      <div>
       </div>
     </div>
   )
