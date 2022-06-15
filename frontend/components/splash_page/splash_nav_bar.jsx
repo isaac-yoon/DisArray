2492c7ca98941e7c6dd91c72050a35cefe29b493
Isaac <iyoon22@gmail.com>
2022-06-14 23:33:11 -0700

    Re-add login button and remove github and linkedin icons

--- frontend/components/splash_page/splash_nav_bar.jsx
+++ frontend/components/splash_page/splash_nav_bar.jsx
@@ -5,26 +5,12 @@ const SplashNavBar = ({ currentUser, logout, openModal }) => {
   return (
     <div className = "main-nav-bar">
       <div className = "main-nav-bar-login-button-container">
-        {/* <Link to="/login" id="main-nav-bar-login-link">
+        <Link to="/login" id="main-nav-bar-login-link">
           <button 
             className = "button" 
             id="main-nav-bar-login-button"
           >LOGIN</button>
-        </Link> */}
-        <div>
-          <img 
-            alt="GitHub"
-            src={window.github}
-            // class="fa-brands"
-            style="width:2em; height:3em"
-          />
-          <img 
-            alt="GitHub"
-            src={window.linkedin}
-            style="width:2em; height:3em"
-            // class="fa-brands"
-          />
-        </div>
+        </Link>
       </div>
 
     </div>
