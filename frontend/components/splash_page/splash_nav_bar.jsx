c880286885b8f161fea6689973e131cb5ee3a47a
Isaac <iyoon22@gmail.com>
2022-06-14 21:45:44 -0700

    Clean up splash page and add in personal links

--- frontend/components/splash_page/splash_nav_bar.jsx
+++ frontend/components/splash_page/splash_nav_bar.jsx
@@ -5,12 +5,16 @@ const SplashNavBar = ({ currentUser, logout, openModal }) => {
   return (
     <div className = "main-nav-bar">
       <div className = "main-nav-bar-login-button-container">
-        <Link to="/login" id="main-nav-bar-login-link">
+        {/* <Link to="/login" id="main-nav-bar-login-link">
           <button 
             className = "button" 
             id="main-nav-bar-login-button"
           >LOGIN</button>
-        </Link>
+        </Link> */}
+        <div>
+          <a class="linkedin-icon" href="https://www.linkedin.com/in/lucidlaughter" target="_blank"></a>
+          <a class="github-icon" href="https://www.github.com/isaac-yoon/DisArray" target="_blank"></a>
+        </div>
       </div>
 
     </div>
